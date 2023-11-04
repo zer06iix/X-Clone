232dfaf69283a8c1c92b74bb819549b06f6206b1
ZER06IIX <akhaksar@gmail.com>
2023-11-04 19:03:11 +0330

    uncompleted changes

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
-posts = [
-    {
-        "author":"Sarah",
-        "title":"spiderman",
-        "content":"post1-content",
-        "date_posted":"1996"
-    },
-    {
-        "author":"Ali",
-        "title":"batman",
-        "content":"post2-content",
-        "date_posted":"2006"
-    }
-]
+# posts = [
+#     {
+#         "author":"Sarah",
+#         "title":"spiderman",
+#         "content":"post1-content",
+#         "date_posted":"1996"
+#     },
+#     {
+#         "author":"Ali",
+#         "title":"batman",
+#         "content":"post2-content",
+#         "date_posted":"2006"
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': 
     }
     return render(request,"blog/home.html", context)
 
