2f9f601a91a3f70ec91a13a7af123fc683dbecf6
ZER06IIX <akhaksar@gmail.com>
2023-11-18 19:08:24 +0330

    uncomplete changes

--- users/views.py
+++ users/views.py
@@ -4,16 +4,17 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
-    if request.method =="POST":
+    if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            # username = form.cleaned_data.get('username')
-            messages.success(request, f"Your account has been created! You are now able to login")
+            messages.success(
+                request, f'Your Account Has Been Created! You Are Now Able To Log In')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
@@ -27,7 +28,7 @@ def profile(request):
             u_form.save()
             p_form.save()
             messages.success(
-                request, f'Your account has been updated!')
+                request, f'Your Account Has Been Updated!')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
